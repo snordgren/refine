9a174f0d54c12a781b4f3f49776704619c5bf189
silasnordgren <nordgrensilas@gmail.com>
2018-12-27 01:45:50 +0100

    Remove unused EscapeHTML

--- refine-core/shared/src/test/scala/refine/EscapeHTMLSpec.scala
+++
@@ -1,8 +0,0 @@
-package refine
-
-class EscapeHTMLSpec extends UnitSpec {
-  "HTML escaping" should "escape necessary characters" in {
-    EscapeHTML.attribute("""&<>"'""") should be("&amp;&lt;&gt;&quot;&#39;")
-    EscapeHTML.element("""&<>""") should be("&amp;&lt;&gt;")
-  }
-}
